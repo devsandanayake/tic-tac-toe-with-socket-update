83da36f23bd3a38bc3e8c570e7ee895a8864b2ef
devin <devinsandanayake49@gmail.com>
2025-01-14 16:52:25 +0530

    up

--- src/app/page.js
+++ src/app/page.js
@@ -161,7 +161,7 @@ const GameRoom = () => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(payload),
+                body: payload,
             });
 
             const result = await response.json();
